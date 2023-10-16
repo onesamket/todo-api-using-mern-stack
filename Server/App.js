a7c689033e65bd587a2cf1d8e8a25dabd7fc94e5
Tewodros Birhanu <onesamket@gmail.com>
2023-10-16 09:06:50 -0700

    initial commit

---
+++ Server/App.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+// app.use(cors());
+app.get('/', (req, res) => {
+    res.send({ message: "Server Running" });
+})
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({ name: req.body.name });
+})
+
+app.listen(PORT, () => console.log("Server Running on localhost:5000"));
---
+++ client/README.md
@@ -0,0 +1,17 @@
+### this is the front part of the application
+
+## available commands
+
+- navigate to client folder
+```
+cd client
+```
+- start the development server
+```
+npm run dev
+```
+- build project for production
+
+```
+npm run build
+```
