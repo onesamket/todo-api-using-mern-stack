549e87130618319b75de8b0405cc5818a4c71385
Tewodros Birhanu <onesmaket@gmail.com>
2023-10-27 07:48:51 -0700

    prisma setup

--- Server/App.js
+++ Server/App.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.get('/', (req, res) => {
     res.send({ message: "Server Running" });
 })
